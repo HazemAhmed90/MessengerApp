8fd20e613df9f6f81fdde659ce7b928e49368fc5
hazem <hazem@LAPTOP-K4S5C927>
2024-03-02 14:04:14 +0000

    Add project files.

---
+++ MessengerApp/Models/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Models
+{
+    public class ChatHub : Hub
+    {
+        private AppDbContext _dbContext;
+
+        public ChatHub(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        public async Task SendMessage(string user, string message)
+        {
+            var chatMessage = new Message
+            {
+                User = user,
+                Content = message,
+                Timestamp = DateTime.UtcNow
+            };
+
+            _dbContext.Messages.Add(chatMessage);
+            await _dbContext.SaveChangesAsync();
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
